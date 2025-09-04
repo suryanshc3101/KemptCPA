407b9335310921f60cadef2a891a4b543ec2b6cd
suryanshc3101 <suryanshc3101@gmail.com>
2025-09-03 23:28:17 -0400

    Make Kempt CPA logo bigger

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ export default function Header() {
             <img 
               src="/dLgjCDsV_XRM_LOGO_BG_png.png" 
               alt="Kempt CPA Logo" 
-             className="w-24 h-24 object-contain"
+             className="w-32 h-32 object-contain"
             />
             <div className="flex flex-col">
               <div className="text-3xl font-bold text-blue-900">
